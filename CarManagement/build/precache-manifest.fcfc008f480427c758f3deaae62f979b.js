5ee97c0477f462c0c724fdc860058e701f30290e
mitpatel131999 <72972092+mitpatel131999@users.noreply.github.com>
2024-12-05 23:22:18 +1000

    added the car managemenr project froent end

---
+++ CarManagement/build/precache-manifest.fcfc008f480427c758f3deaae62f979b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7084c82bb0619e299e5ff3ec90af7f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5db0032fcddc5c557c",
+    "url": "/static/css/2.f5eb4559.chunk.css"
+  },
+  {
+    "revision": "dc545422c2abd20e867a",
+    "url": "/static/css/main.ef6f81d8.chunk.css"
+  },
+  {
+    "revision": "4f5db0032fcddc5c557c",
+    "url": "/static/js/2.bf137abd.chunk.js"
+  },
+  {
+    "revision": "2143b933c7a959228273cf1325c6ade4",
+    "url": "/static/js/2.bf137abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc545422c2abd20e867a",
+    "url": "/static/js/main.ab6b62d5.chunk.js"
+  },
+  {
+    "revision": "377d4700f97d84f210a3",
+    "url": "/static/js/runtime-main.a15ca8a1.js"
+  }
+]);
